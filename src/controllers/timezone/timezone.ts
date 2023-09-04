9e562b4eff85c07818b261bb6eebb11a226f578d
Martin Bobbio <martinbobbio1@gmail.com>
2023-09-04 12:32:01 -0300

    add: uuid for improve persistence

--- src/controllers/timezone/timezone.ts
+++ src/controllers/timezone/timezone.ts
@@ -2,6 +2,12 @@
 import { NextFunction, Request, Response } from "express";
 // Constants
 import { TimezoneDetailData, apiWorldtime, redis } from "../../constants";
+// Unique id
+import { v4 as uuidv4 } from "uuid";
+
+const uniqueId = uuidv4();
+// Maybe is a better aproach to move to a file the id for a better persitence
+const collectionTimezones = `clocks/${uniqueId}`;
 
 /**
  * Controller for retrieving timezones list from world time api.
@@ -22,8 +28,9 @@ export const getTimezoneList = async (
     const data = !cachedResponse
       ? (await apiWorldtime.get<string[]>("/timezone")).data
       : cachedResponse;
-    const current = await redis.smembers("clocks");
+    const current = await redis.smembers(collectionTimezones);
     const list = data.filter((element) => !current.includes(element));
+    // Cache Response for 1 Hour for avoid errors or rate limits (move this time to .env)
     await redis.set("timezoneApi", JSON.stringify(data), "EX", 3600);
     res.status(200).json({ list, current });
   } catch (error) {
@@ -74,8 +81,10 @@ export const setTimezone = async (
 ) => {
   try {
     const { name } = req.params;
-    await redis.sadd("clocks", name);
-    res.status(200).json({ message: `${name} added to clocks.` });
+    await redis.sadd(collectionTimezones, name);
+    res
+      .status(200)
+      .json({ message: `${name} added to ${collectionTimezones}.` });
   } catch (error) {
     console.log(JSON.stringify(error));
     next(error);
@@ -98,8 +107,10 @@ export const deleteTimezone = async (
 ) => {
   try {
     const { name } = req.params;
-    await redis.srem("clocks", name);
-    res.status(200).json({ message: `${name} deleted of clocks.` });
+    await redis.srem(collectionTimezones, name);
+    res
+      .status(200)
+      .json({ message: `${name} deleted of ${collectionTimezones}.` });
   } catch (error) {
     console.log(JSON.stringify(error));
     next(error);
